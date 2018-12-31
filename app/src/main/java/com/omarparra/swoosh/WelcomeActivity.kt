67ad606dd44ef4da9335b36d67faa23758d5e4b9
Omar Parra <omarfdez92@gmail.com>
2018-12-31 16:47:31 -0600

    add the setOnClickListener to explicitly intent to change the layout

--- app/src/main/java/com/omarparra/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/omarparra/swoosh/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package com.omarparra.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStarterBtn.setOnClickListener {
+            //explicit intent ... explicit because we know where we going
+             val leagueIntent =  Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
